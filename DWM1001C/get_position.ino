252e6499788d147cf01e8952e718e54e946cb5aa
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2025-06-16 17:13:53 +0700

    Create get_position.ino

---
+++ DWM1001C/get_position.ino
@@ -0,0 +1,63 @@
+void sent_pos_get_protocols()
+{
+  byte pos_get[] = {0x02, 0x00};
+  Serial2.write(pos_get, sizeof(pos_get));
+  delay(100);
+}
+
+float* pos_get()
+{ 
+  int Time_out = millis();
+  sent_pos_get_protocols();
+
+  while (!Serial2.available())
+  {
+    if(millis() - Time_out > 5000)
+    {
+      Time_out = millis();
+      sent_pos_get_protocols();
+      
+      //Serial.println("Time_out");
+    }
+  }
+
+  float* position_XYZ = (float*)malloc(3*sizeof(float));
+
+  position_XYZ[0] = 0.0;
+  position_XYZ[1] = 0.0;
+  position_XYZ[2] = 0.0;
+
+  byte idx = 0, TLV[20];
+  for(int i=0;i<20;i++) TLV[i] = 0x00;
+
+  do
+  {
+    byte res = Serial2.read();
+    if(idx > 0 || res == 0x40)
+    {
+      TLV[idx] = res;
+      idx++;
+    }
+  }while (Serial2.available() && idx < 20);
+
+
+  if(valid_protocol(TLV))
+  {
+    /*for(int i=0;i<18;i++)
+    {
+      Serial.print(TLV[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println();*/
+
+    x = TLV[5]  | (TLV[6] << 8)  | (TLV[7] << 16)  | (TLV[8] << 24);
+    y = TLV[9]  | (TLV[10] << 8) | (TLV[11] << 16) | (TLV[12] << 24);
+    z = TLV[13] | (TLV[14] << 8) | (TLV[15] << 16) | (TLV[16] << 24);
+
+
+    position_XYZ[0] = x/1000.0;
+    position_XYZ[1] = y/1000.0;
+    position_XYZ[2] = z/1000.0;
+  }
+  return position_XYZ;
+}
