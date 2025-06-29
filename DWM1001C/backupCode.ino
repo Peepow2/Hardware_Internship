2d20a04cd3e1d6f16f2bc864a270197ee58e7fbb
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2025-06-30 00:29:52 +0700

    Update backupCode.ino

--- DWM1001C/backupCode.ino
+++ DWM1001C/backupCode.ino
@@ -5,8 +5,10 @@
 
 byte cmd_Check_Valid_response[] = {0x40, 0x01, 0x00};
 
+byte cmd_set_pos[] = {0x01, 0x0D};      // not in Tag mode
 byte cmd_getposTAG[] = {0x02, 0x00};    // Tag Position
 byte cmd_getposAnchor[] = {0x0C, 0x00}; // All location
+byte cmd_get_Anchor_list[] = {0x0B,0x01,0x01};
 
 byte cmd_sleep[] = {0x0A, 0x00};
 byte cmd_reset[] = {0x14, 0x00};
@@ -15,8 +17,7 @@ byte cmd_factory_reset[] = {0x13, 0x00};
 byte cmd_set_to_TAG[] = {0x05, 0x02, 0xCE, 0x00};
 byte cmd_set_to_Anchor[] = {0x07, 0x02, 0x9E, 0x00};
 
-byte cmd_get_Anchor_list[] = {0x0B,0x01,0x01};
-
+byte cmd_get_config[] = {0x08, 0x00};
 
 byte* sent_cmd(byte cmd[])
 {
@@ -24,17 +25,18 @@ byte* sent_cmd(byte cmd[])
 
   unsigned long LastSent = millis();   // เวลาสำหรับการส่งใหม่
   unsigned long begin = millis();      // เวลาตั้งแต่เริ่มเรียกฟังก์ชัน
-  unsigned long NOW = 0;
+  unsigned long NOW = millis();
   unsigned long diff_from_LastSent = 0;
   unsigned long diff_from_begin = 0;
 
-  
-  while(!Serial2.available())
+  Serial2.write(cmd, sizeof(cmd));
+  delay(100);
+
+  /*while(!Serial2.available())
   {
     NOW = millis();
-    diff_from_LastSent = \
-        (NOW >= LastSent) ? (NOW - LastSent) : \
-        (ULONG_MAX - LastSent + NOW);
+    diff_from_LastSent = NOW - LastSent;
+    diff_from_begin    = NOW - begin;
 
     if(diff_from_LastSent > 5000)
     {
@@ -43,15 +45,12 @@ byte* sent_cmd(byte cmd[])
       Serial.println(diff_from_LastSent);
     }
 
-    diff_from_begin = \
-        (NOW > begin) ? NOW - begin : ULONG_MAX - begin + NOW;
-
-    if(diff_from_begin > 60000) // 1 min
+    else if(diff_from_begin > 60000) // 1 min
     {
       // Something to stop everything
       Serial.println("End");
     }
-  }
+  }*/
 
   byte index = 0;
   byte *TLV = (byte*)malloc(ByteArrSize * sizeof(byte));
@@ -114,7 +113,7 @@ void Reset()
   free(TLV);
 }
 
-void factory_reset()
+bool factory_reset()
 {
   byte *TLV = sent_cmd(cmd_factory_reset);
   if(cmd_Check_Valid_response[0] == TLV[0] \
@@ -122,20 +121,63 @@ void factory_reset()
     && cmd_Check_Valid_response[2] == TLV[2])
   {
     Serial.println("Factory_reset Complete");
+    return true;
   }
   free(TLV);
+  return false;
+}
+
+void set_label(String Label)
+{
+  byte *cmd_label = (byte*)malloc((len+2)*sizeof(byte)); // {0x1D, Label_Length, Label}
+  cmd_label[0] = 0x1D;
+  cmd_label[1] = Label.length();
+  
+  byte len = Label.length();
+  for(byte i=0;i<len;i++)
+  {
+    cmd_label[i+2] = int(Label.charAt(i));
+  }
+  for(byte i=0;i<len+2;i++)
+  {
+    Serial.print(cmd_label[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+  byte *TLV = sent_cmd(cmd_label);
+  free(TLV);
+}
+
+void get_config()
+{
+  byte *TLV = sent_cmd(cmd_get_config);
+  byte config = TLV[];
+  if(config & 0010000 == 0010000)
+    // tag ;
+  else
+    // anchor ;
+}
+
+void set_To_TAG()
+{
+
+}
+
+void set_To_Anchor()
+{
+  
 }
 
 void setup() 
 {
   Serial.begin(baud_rate);
   Serial2.begin(baud_rate, SERIAL_8N1, RX, TX);
+
 }
 
 void loop()
 {
-  float* Position_XYZ = pos_get();
-
+  //float* Position_XYZ = pos_get();
   //loc_get();
   
   /*for(byte i=0;i<3;i++)
@@ -144,5 +186,5 @@ void loop()
     Serial.print(" ");
   }
   Serial.println();*/
-  delay(2000);
+  set_label("RourkeEasy");
 }
