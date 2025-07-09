7abe3fc0304db08d990dab71ab4d0f6baf1d3559
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2025-07-09 11:16:37 +0700

    Update get_position.ino

--- DWM1001c-function/get_position.ino
+++ DWM1001c-function/get_position.ino
@@ -3,7 +3,8 @@ byte cmd_getposTAG[] = {0x02, 0x00};
 float* get_pos()
 {
   byte* TLV = send_cmd(cmd_getposTAG);
-  
+  if(TLV == NULL) return NULL;
+
   float* POS = (float*)malloc(3*sizeof(float));
   POS[0] = (TLV[5]  |  TLV[6] << 8 |  TLV[7] << 16 |  TLV[8] << 24) / 1000.0;
   POS[1] = (TLV[9]  | TLV[10] << 8 | TLV[11] << 16 | TLV[12] << 24) / 1000.0;
