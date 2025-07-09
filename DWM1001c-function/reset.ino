ee4c5ee453a9461b9787763d324c1fef887df29b
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2025-07-09 11:25:56 +0700

    Update reset.ino

--- DWM1001c-function/reset.ino
+++ DWM1001c-function/reset.ino
@@ -1,10 +1,18 @@
 byte cmd_reset[] = {0x14, 0x00};
+byte cmd_factory_reset[] = {0x13, 0x00};
+
 bool reset()
 {
-  bool complete = false;
-  byte* TLV_reset = send_cmd(cmd_reset);
-  if(TLV_reset != NULL)
-    complete = true;
-  free(TLV_reset);
-  return complete;
+  byte* TLV = send_cmd(cmd_reset);
+  if(TLV == NULL) return false;
+  free(TLV);
+  return true;
+}
+
+bool factory_reset()
+{
+  byte* TLV = send_cmd(cmd_factory_reset);
+  if(TLV == NULL) return false;
+  free(TLV);
+  return true;
 }
