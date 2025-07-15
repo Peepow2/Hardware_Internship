05c8c2c9db6f20ef652907d07a2249bd96c71175
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2025-07-15 09:51:30 +0700

    Create printToDisplay.ino

---
+++ ESP32_LCD_module/printToDisplay.ino
@@ -0,0 +1,37 @@
+#include <TFT_eSPI.h>
+
+TFT_eSPI tft = TFT_eSPI();
+
+#define SCREEN_WIDTH 240
+#define SCREEN_HEIGHT 320
+#define FONT_SIZE 2
+
+void printToDisplay(string S) 
+{
+  tft.fillScreen(TFT_WHITE);
+  tft.setTextColor(TFT_BLACK);
+
+  int centerX = SCREEN_WIDTH / 2;
+  int centerY = SCREEN_HEIGHT / 2;
+
+  tft.drawCentreString(S, centerX, centerY, FONT_SIZE);
+}
+
+void setup() 
+{
+  Serial.begin(115200);
+
+  tft.init();
+  tft.setRotation(2);
+
+  tft.fillScreen(TFT_WHITE);
+  tft.setTextColor(TFT_BLACK);
+  
+  int centerX = SCREEN_WIDTH / 2;
+  int centerY = SCREEN_HEIGHT / 2;
+}
+
+void loop() 
+{
+  printToDisplay(millis());
+}
